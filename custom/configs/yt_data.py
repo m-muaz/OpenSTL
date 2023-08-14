c5d50449579f4de1bfaba733b2b75c2f24716247
hzy0211 <zyhe@utexas.edu>
2023-08-14 17:19:01 -0500

    Fix environment issue and dataloader issue

--- custom/configs/yt_data.py
+++ custom/configs/yt_data.py
@@ -6,10 +6,10 @@ val_root = "/mnt/sda1/ted_video/youtube-categories/ExtractedData/Interview/test"
 train_gs = "/mnt/sda1/ted_video/youtube-categories/ExtractedData/Interview/train"
 val_gs = "/mnt/sda1/ted_video/youtube-categories/ExtractedData/Interview/test"
 data_threads = 8
-image_height=32
-image_width=32
+image_height=512
+image_width=512
 
 # model hyperparameters
-n_past = 3
+n_past = 2
 n_future = 1
 n_eval = 3
