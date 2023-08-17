99245e8a872a7458a4c57a994167c88565a215c3
Muaz Hester <21100231@lums.edu.pk>
2023-08-17 16:19:09 -0500

    Changed data directories in yt_data config, changed # of epochs for different model testing, fixed some bugs in modelAnalysis script

--- custom/configs/yt_data.py
+++ custom/configs/yt_data.py
@@ -2,10 +2,10 @@
 dataname = "custom_yt"
 val_batch_size =  1
 batch_size = 2
-train_root = "/mnt/sda1/ted_video/youtube-categories/ExtractedData/Interview/train"
-val_root = "/mnt/sda1/ted_video/youtube-categories/ExtractedData/Interview/test"
-train_gs = "/mnt/sda1/ted_video/youtube-categories/ExtractedData/Interview/train"
-val_gs = "/mnt/sda1/ted_video/youtube-categories/ExtractedData/Interview/test"
+train_root = "/data/data1/muaz/ExtractedData/Interview/train"
+val_root = "/data/data1/muaz/ExtractedData/Interview/test"
+train_gs = "/data/data1/muaz/ExtractedData/Interview/train"
+val_gs = "/data/data1/muaz/ExtractedData/Interview/test"
 data_threads = 8
 image_height=512
 image_width=512
--- custom/modelAnalysis.py
+++ custom/modelAnalysis.py
@@ -1,13 +1,32 @@
+import os
 import warnings
 warnings.filterwarnings("ignore")
 import numpy as np 
 from openstl.utils import (show_video_line, show_video_gif_multiple)
-from openstl.datasets import dataset_parameters
+from custom.utils import load_dtparser, update_config
+from openstl.utils import load_config
+
+class Config:
+    def __init__(self, *args):
+        self.__dict__.update(*args)
+
+args = load_dtparser().parse_args()
+opt = args.__dict__
+# Create load_config object
+config = update_config(opt, load_config("./custom/configs/yt_data.py"))
+config = Config(config)
+
+# saved model directory
+saved_model_dir = './work_dirs/custom_yt_data_100_epochs_SimVP_gSTA/saved'
+
+# make images directory under the parent of saved model directory
+image_dir = os.path.join(saved_model_dir, '../images')
+os.makedirs(image_dir, exist_ok=True)
 
 # show the given frames 
-inputs = np.load('../work_dirs/mmnist_simvp_gsta_200_epochs/saved/inputs.npy')
-preds = np.load('../work_dirs/mmnist_simvp_gsta_200_epochs/saved/preds.npy')
-trues = np.load('../work_dirs/mmnist_simvp_gsta_200_epochs/saved/trues.npy')
+inputs = np.load(saved_model_dir + '/inputs.npy')
+preds = np.load(saved_model_dir + '/preds.npy')
+trues = np.load(saved_model_dir + '/trues.npy')
 
 
 # check the dimensions of the inputs, preds and trues
@@ -17,24 +36,28 @@ print(trues.shape)
 
 
 # number of input and output frames
-pre_seq_length = dataset_parameters["mmnist"]["pre_seq_length"]
-aft_seq_length = dataset_parameters["mmnist"]["aft_seq_length"]
+pre_seq_length = config.n_past
+aft_seq_length = config.n_future
 
 # show the video frames for a random sample
 
 # displaying input frames
 sample_idx = np.random.randint(0, inputs.shape[0])
-show_video_line(inputs[sample_idx], ncols=pre_seq_length, vmax=0.6, cbar=False, out_path='inputs_200.png',\
-                format='png', use_rgb=True)
+show_video_line(inputs[sample_idx], ncols=pre_seq_length, vmax=0.6, cbar=False,\
+                 out_path= image_dir + f'/inputs_{sample_idx}.png',\
+                 format='png', use_rgb=True)
 
 # displaying predicted frames
-show_video_line(preds[sample_idx], ncols=pre_seq_length, vmax=0.6, cbar=False, out_path='preds_200.png',\
+show_video_line(preds[sample_idx], ncols=aft_seq_length, vmax=0.6, cbar=False,\
+                out_path= image_dir + f'/preds_{sample_idx}.png',\
                 format='png', use_rgb=True)
 
 
 # displaying ground truth frames
-show_video_line(trues[sample_idx], ncols=pre_seq_length, vmax=0.6, cbar=False, out_path='trues_200.png',\
+show_video_line(trues[sample_idx], ncols=aft_seq_length, vmax=0.6, cbar=False,\
+                out_path= image_dir + f'/trues_{sample_idx}.png',\
                 format='png', use_rgb=True)
 
-# generate gif fil based on the input, predicted and ground truth frames
-show_video_gif_multiple(inputs[sample_idx], trues[sample_idx], preds[sample_idx], out_path='mnist_simvp_epochs_200.gif', use_rgb=True)
+# # generate gif fil based on the input, predicted and ground truth frames
+# show_video_gif_multiple(inputs[sample_idx], trues[sample_idx], preds[sample_idx],\
+#                          out_path=image_dir + f'custom_yt_Interview_{sample_idx}.gif', use_rgb=True)
