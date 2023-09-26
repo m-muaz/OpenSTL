Fixed mean, std dev computation issues; added routine to print traniability of model; plus fixed other bugs and minor changes

openstl/api/train.py
@@ -250,7 +250,7 @@ class BaseExperiment(object):
 
     def display_method_info(self):
         """Plot the basic infomation of supported methods"""
-        T, C, H, W, _, _, _ = self.args.in_shape
+        T, C, H, W, _, _, _, _ = self.args.in_shape
         num_ad_frames = int(self.train_loader.dataset.audio_sample_rate * (1 / self.train_loader.dataset.video_frame_rate))
         if self.args.method in ['simvp', 'tau']:
             input_dummy = torch.ones(1, self.args.pre_seq_length, C, H, W).to(self.device)
@@ -293,6 +293,22 @@ class BaseExperiment(object):
         else:
             fps = ''
         print_log('Model info:\n' + info+'\n' + flops+'\n' + fps + dash_line)
+
+        # print if model layers are trainable or not
+        for name, sub_module in self.method.model.named_children():
+            print_log(f"Module: {name}")
+            self.print_trainability(sub_module, "  ")
+
+
+    
+    def print_trainability(self, model, indent=""):
+        for name, param in model.named_parameters():
+            trainable = param.requires_grad
+            print_log(f"{indent}Layer: {name}, Trainable: {trainable}")
+        
+        for name, sub_module in model.named_children():
+            print_log(f"{indent}Module: {name}")
+            self.print_trainability(sub_module, indent + "  ")
 
     def train(self):
         """Training loops of STL methods"""

openstl/models/simvp_model.py
@@ -1,4 +1,8 @@
 import torch
+
+# Enable anomaly detection
+torch.autograd.set_detect_anomaly(True)
+
 import torchaudio
 from torch import nn
 
@@ -19,7 +23,7 @@ class SimVP_Model(nn.Module):
                  mlp_ratio=8., drop=0.0, drop_path=0.0, spatio_kernel_enc=3,
                  spatio_kernel_dec=3, act_inplace=True, **kwargs):
         super(SimVP_Model, self).__init__()
-        T, C, H, W, _, _, _ = in_shape  # T is pre_seq_length
+        T, C, H, W, _, _, _, _ = in_shape  # T is pre_seq_length
         H, W = int(H / 2**(N_S/2)), int(W / 2**(N_S/2))  # downsample 1 / 2**(N_S/2)
         act_inplace = False
         self.enc = Encoder(C, hid_S, N_S, spatio_kernel_enc, act_inplace=act_inplace)
@@ -27,7 +31,7 @@ class SimVP_Model(nn.Module):
         self.dec = Decoder(hid_S+2, hid_S, C, N_S, spatio_kernel_dec, act_inplace=act_inplace)
 
         # Creating object for audio feature extraction
-        T_, C_, H_, W_, ad_prev_frames, audio_sample_rate, video_frame_rate = in_shape
+        T_, C_, H_, W_, ad_prev_frames, ad_future_frames, audio_sample_rate, video_frame_rate = in_shape
         _, _C, _H, _W, = self.compute_enc_yshape(torch.ones(1, T_, C_, H_, W_))
 
         self.ad_feat_extractor = torchaudio.pipelines.WAV2VEC2_BASE.get_model()
@@ -38,7 +42,7 @@ class SimVP_Model(nn.Module):
         # Compute input feature shape for AudioMLP
         if audio_sample_rate is not None and video_frame_rate is not None:
             audio_frame = int(audio_sample_rate * (1/video_frame_rate))
-            out = self.compute_audio_shape(torch.ones(1, T, 2, audio_frame * (ad_prev_frames + T_ + 1)))
+            out = self.compute_audio_shape(torch.ones(1, T, 2, audio_frame * (ad_prev_frames + ad_future_frames + 1)))
             self.ad_net = AudioMLP(out, T, _H, _W)
         else:
             self.ad_net = AudioMLP(768*9, T, _H, _W)