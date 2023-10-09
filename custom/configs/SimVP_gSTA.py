a346c60186b394040abe44d338fd86a76cbd889d
Muaz Hester <21100231@lums.edu.pk>
2023-10-08 22:28:17 -0500

    Fixed issue for additional ad future frames and changed config

--- custom/configs/SimVP_gSTA.py
+++ custom/configs/SimVP_gSTA.py
@@ -12,7 +12,7 @@ fp16 = False
 lr = 1e-4
 drop_path = 0
 sched = "onecycle"
-epoch = 10
+epoch = 25
 log_step = 1
 # model checkpoint
-ex_name = f"custom_yt_data_normalized_{epoch}_epochs_InputOutput_{5}_{10}_withFutureAudio_SimVP_gSTA"
+ex_name = f"custom_yt_data_normalized_{epoch}_epochs_InputOutput_{5}_{5}_withFutureAudio_{10}_SimVP_gSTA_v1"
--- custom/configs/yt_data.py
+++ custom/configs/yt_data.py
@@ -1,10 +1,10 @@
 # dataloaders for yt data
 dataname = "custom_yt"
-val_batch_size =  1
-batch_size = 2
-train_root = "/mnt/sda1/muaz/ted_video/youtube-categories/ExtractedData/Interview/train"
-val_root = "/mnt/sda1/muaz/ted_video/youtube-categories/ExtractedData/Interview/val"
-test_root = "/mnt/sda1/muaz/ted_video/youtube-categories/ExtractedData/Interview/test"
+val_batch_size =  3
+batch_size = 5
+train_root = "/data/data1/muaz/remote_fs/ted_video/youtube-categories/ExtractedData/Interview/train"
+val_root = "/data/data1/muaz/remote_fs/ted_video/youtube-categories/ExtractedData/Interview/val"
+test_root = "/data/data1/muaz/remote_fs/ted_video/youtube-categories/ExtractedData/Interview/test"
 # train_gs = "/mnt/sda1/muaz/ted_video/youtube-categories/ExtractedData/Interview/train"
 # val_gs = "/mnt/sda1/muaz/ted_video/youtube-categories/ExtractedData/Interview/test"
 data_threads = 8
@@ -18,7 +18,7 @@ n_eval = n_future
 
 # parameter to use previous audio features
 ad_prev_frames = 0
-ad_future_frames = 10
+ad_future_frames = 5
 
 # save the model statistics under this folder
 model_save_path = "custom/data_stats/"
